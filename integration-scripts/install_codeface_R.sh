84740f83692a2ec130fb669e73e9c3b5e17b93d4
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-19 18:13:12 +0200

    install R 3.6.3 and handle dependecies
    Cause of R 3.6.3 we needed a different ppa ppa:marutter/c2d4u3.5,
     apt instead of apt-get for core modules and r-cran-rjava,
     added ppa:cran/poppler cause of rpoppler error and changed new
     dependencies for gdtools, units R packages

--- integration-scripts/install_codeface_R.sh
+++ integration-scripts/install_codeface_R.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 # Copyright Roger Meier <roger@bufferoverflow.ch>
-# SPDX-License-Identifier:	Apache-2.0 BSD-2-Clause GPL-2.0+ MIT WTFPL
+# SPDX-License-Identifier:	Apache-2.0 BSD-2-Clause GPL-2.0+ MIT WTtPL
 
 echo "Providing R libraries"
+# try sudo add-apt-repository ppa:marutter/c2d4u3.5
+#sudo apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install r-base r-base-dev r-base-core 
+sudo DEBIAN_FRONTEND=noninteractive apt -qy install r-base r-base-dev r-base-core
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install r-base r-base-dev
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:marutter/c2d4u3.5
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qy update  
 
-sudo R CMD javareconf
+#sudo R CMD javareconf
+sudo apt install -qy r-cran-rjava
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install \
+sudo DEBIAN_FRONTEND=noninteractive apt install -qy \
 	r-cran-zoo r-cran-xts \
 	r-cran-xtable r-cran-reshape r-cran-stringr r-cran-scales \
-	r-cran-scales r-cran-rmysql r-cran-rcurl r-cran-mgcv \
-	r-cran-rjson r-cran-testthat libx11-dev libssl-dev libssh2-1-dev
+	r-cran-scales r-cran-rmysql \
+	r-cran-rjson r-cran-testthat
 
 echo "Providing R libraries - packages.r"
 
+# added because Required package curl not found
+sudo DEBIAN_FRONTEND=noninteractive apt install -qy libcurl4-openssl-dev libfontconfig1-dev libssl-dev libxml2-dev
+# added because Rpoppler error
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:cran/poppler
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qy update
+sudo DEBIAN_FRONTEND=noninteractive apt install -qy libpoppler-cpp-dev
+# added because gdtools, units, error
+sudo DEBIAN_FRONTEND=noninteractive apt install -qy libcairo2-dev libudunits2-dev libmagick++-dev libgeos-dev libgdal-dev
+
 sudo Rscript packages.r
+# for test
+echo "Test reinstall.package.from.github"
