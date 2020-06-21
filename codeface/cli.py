ca02d0f21923e6698d2d1617a875bae2bf37fe10
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-21 18:44:20 +0200

    codeface: cli: Add shebang for python 3
    We added a shebang for Python 3 default.

--- codeface/cli.py
+++ codeface/cli.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 ## This file is part of Codeface. Codeface is free software: you can
 ## redistribute it and/or modify it under the terms of the GNU General Public
 ## License as published by the Free Software Foundation, version 2.
