f4d1aa73c339b252167d092312b14a318795dd60
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-21 16:12:04 +0200

    install_repositories: change debian frontend to python 3 and cran repositority to Ubuntu bionic
    We update to python 3 in ubuntu bionic, hence we changed the debian
     frontend to python 3 instead of python 2 and the cran repositority to Ubuntu bionic.

--- integration-scripts/install_repositories.sh
+++ integration-scripts/install_repositories.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 
 sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install software-properties-common python-software-properties
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install software-properties-common python3-software-properties
 
 echo "Adding R cran repositories"
 version=`lsb_release -r | awk '{ print $2;}'`
 
 case ${version} in
-    "14.04")
-	echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" | sudo tee -a /etc/apt/sources.list
-	;;
-    "16.04")
-	echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+    "18.04")
+	echo "deb http://cran.rstudio.com/bin/linux/ubuntu bionic-cran35/" | sudo tee -a /etc/apt/sources.list
 	;;
     *) echo "Unsupported version of Ubuntu detected, aborting"
        exit 1;;
 esac
 
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
+gpg --keyserver keyserver.ubuntu.com --recv-key 51716619E084DAB9
+gpg -a --export 51716619E084DAB9 | sudo apt-key add -
 
 #echo "Adding node.js repository"
 #sudo add-apt-repository -y ppa:chris-lea/node.js
 
 sudo apt-get update -qq
-
