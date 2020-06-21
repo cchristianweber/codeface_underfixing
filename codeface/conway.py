630ae66a868f179ba8811973e7ccc85cd0baf395
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-21 19:00:41 +0200

    codeface: conway: Add shebang for Python 3
    We added a shebang for Python 3 default.

--- codeface/conway.py
+++ codeface/conway.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 
 # This file is part of Codeface. Codeface is free software: you can
 # redistribute it and/or modify it under the terms of the GNU General Public
