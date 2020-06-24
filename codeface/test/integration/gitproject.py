fce190ec91bc69d6a0aa67abe464a9391b144523
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-24 07:12:36 +0200

    codeface: test: integration: Add shebang for Python 3, fix Python 2 to 3 function name changes
    We added a shebang for Python 3 support, replaced iteritems() by items() and file() by open() due to change from Python 2 to 3.

--- codeface/test/integration/gitproject.py
+++ codeface/test/integration/gitproject.py
@@ -93,11 +93,15 @@ class GitProject(object):
                         else:
                             unlink(f)
                 # Insert the files specified in the commits filetree
-                for f, content in c.filetree.iteritems():
+                # python 2 to 3 error 'dict' object has no attribute 'iteritems'
+                # for f, content in c.filetree.iteritems():
+                for f, content in c.filetree.items():
                     dn, fn = pathsplit(f)
                     if dn and not exists(dn):
                         makedirs(dn)
-                    with file(f, "w") as fd:
+                    # python 2 to 3 error file() to open() 
+                    #with file(f, "w") as fd:
+                    with open(f, "w") as fd:
                         fd.write(content)
                 # Perform the commit
                 git("add -A .".split())
@@ -158,10 +162,14 @@ class GitProject(object):
                        project=basename(self.directory)
                 )
             )
-            with file(self.codeface_conf, "w") as fd:
+            # python 2 to 3
+            #with file(self.codeface_conf, "w") as fd:
+            with open(self.codeface_conf, "w") as fd:
                 fd.write(configuration)
             for ml_name, ml_file in self.mboxes:
-                with file(ml_file, "w") as fd:
+                # python 2 to 3
+                #with file(ml_file, "w") as fd:
+                with open(ml_file, "w") as fd:
                     fd.write(self.mbox_contents(ml_name))
         finally:
             chdir(cwd)
