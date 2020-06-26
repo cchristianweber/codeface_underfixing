5d99872290952796bb71fc50f80b6d9cebbf1b53
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-26 17:17:07 +0200

    integration-scripts: install_codeface_python: Change Python 2 to 3 setup and install mysqlclient via pip3
    We added Python 3 package setup installation and installed new package mysqlclient via new pip3.

--- integration-scripts/install_codeface_python.sh
+++ integration-scripts/install_codeface_python.sh
@@ -4,11 +4,18 @@
 
 echo "Providing codeface python"
 
-sudo pip install --upgrade -q setuptools
-sudo pip install --upgrade -q mock
-sudo pip install --upgrade -q subprocess32
+# permission error codeface.egg-info/requires.txt
+#sudo chmod u+rx codeface.egg-info/requires.txt
 
-# Only development mode works
-# install fails due to R scripts accessing unbundled resources!
-# TODO Fix the R scripts
-sudo python2.7 setup.py -q develop
+# replace with apt install python3-setuptools
+#sudo pip install --upgrade -q setuptools
+#sudo pip install --upgrade -q mock
+#sudo pip install --upgrade -q subprocess32
+# for package replacement 
+sudo apt install python3-setuptools python3-dev default-libmysqlclient-dev build-essential python3-pip
+
+sudo pip3 install mysqlclient
+#sudo python2.7 setup.py -q develop
+
+sudo python3 setup.py -q develop
+
