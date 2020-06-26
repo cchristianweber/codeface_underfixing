dd605e93d2339d2896457a56fc52962580481dbb
Christian Weber <christian2.weber@stud.uni-regenburg.de>
2020-06-26 17:41:42 +0200

    codeface: runCli: Delete this file
    We deleted this file cause it is not used anymore.

--- codeface/runCli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Simple script to run codeface directly from source
-"""
-import sys
-from codeface.cli import main; 
-if __name__ == '__main__':
-    ret = main()
-    sys.exit(ret)
